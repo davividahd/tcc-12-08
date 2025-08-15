477ef78e4172b457a38ee9d18f49b9414a122c97
davividahd <daviricardosantos2007@gmail.com>
2025-08-15 15:54:04 -0300

    Update editarEmpresa.js

---
+++ SW-19-05-main/src/DAO/Empresa/editarEmpresa.js
@@ -0,0 +1,25 @@
+const {conexao} = require('../conexao.js')
+
+async function editarEmpresa(codigo, campo, valor){
+    const data = [valor, codigo]
+
+    const colunasPermitidas = ['id', 'nome', 'cnpj', 'cidade', 'estado']; // Adicione as colunas permitidas
+    if (!colunasPermitidas.includes(campo)) {
+        throw new Error('Coluna inválida');
+    }
+
+    const sql = `UPDATE tbl_empresa set ${campo} = ? WHERE codigo = ? ;`
+    const conn = await conexao()
+    
+    try {
+        // Executar a consulta
+        const [results] = await conn.query(sql, data);
+
+        await conn.end()
+        return results
+      } catch (err) {
+        return err.message
+      }
+}
+
+module.exports = {editarEmpresa}
